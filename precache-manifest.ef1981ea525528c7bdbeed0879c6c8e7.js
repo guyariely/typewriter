5ce3de8e7ff55088ad025a40065d708c1ee465e1
Guy Arieli <guyarieli1997@gmail.com>
2019-06-26 19:04:54 +0300

    Updates

---
+++ precache-manifest.ef1981ea525528c7bdbeed0879c6c8e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5518ce9e2b36e814a3cd1cb5d6cfa83",
+    "url": "/typewriter/index.html"
+  },
+  {
+    "revision": "246b74d71aae161f6d04",
+    "url": "/typewriter/static/css/2.1cbcbd45.chunk.css"
+  },
+  {
+    "revision": "519b2b01fdb3be786e2d",
+    "url": "/typewriter/static/css/main.1f174e09.chunk.css"
+  },
+  {
+    "revision": "246b74d71aae161f6d04",
+    "url": "/typewriter/static/js/2.866b3a54.chunk.js"
+  },
+  {
+    "revision": "519b2b01fdb3be786e2d",
+    "url": "/typewriter/static/js/main.0b2e821f.chunk.js"
+  },
+  {
+    "revision": "6141be169b307a2ae150",
+    "url": "/typewriter/static/js/runtime~main.1f9e0057.js"
+  },
+  {
+    "revision": "0a397e69acf22237d4a437a62d8d131d",
+    "url": "/typewriter/static/media/vintage-typewriter.0a397e69.jpg"
+  }
+]);
